9f6a91f9adac332a3287168dbf862a7e8762b97a
Vipul Krishnan M. D <vipulkrishnanmd@gmail.com>
2017-05-01 19:08:55 +1000

    Update string.h

--- OS/include/string.h
+++ OS/include/string.h
@@ -9,7 +9,7 @@ uint16 strlength(string ch)
         return --i;
 }
 
-/* Added in episode 3*/ /*This function compares two strings and returns true (1) if they are equal or false (0) if they are not equal */
+/* This function compares two strings and returns true (1) if they are equal or false (0) if they are not equal */
 
 uint8 strEql(string ch1,string ch2)                     
 {
